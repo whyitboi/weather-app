2d5692a9d1739df4e02d9dad21f80b8811fe4fce
whyitboi <20414206+whyitboi@users.noreply.github.com>
2026-09-15 15:49:04 +0300

    Add getWeather function to get the weather data a s.json

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
-import "./style.css";
-import { domLoad } from "./domLoad.js";
+const btn = document.querySelector("button");
+const search = document.getElementById("cats");
 
-domLoad();
+async function getWeather(location) {
+  const response = await fetch(
+    "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/" +
+      encodeURIComponent(location) +
+      "?unitGroup=us&key=DM97HHAYYJMKM9SQ5X2U985K4&contentType=json",
+  );
+
+  const weatherData = await response.json();
+  console.log(weatherData);
+}
+
+btn.addEventListener("click", () => {
+  getWeather(search.value);
+});
