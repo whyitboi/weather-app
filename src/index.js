be6c13f4e84d63ef2633cf99f9a3e639d63b45ad
whyitboi <20414206+whyitboi@users.noreply.github.com>
2026-09-15 16:48:20 +0300

    Add the function to fetch from WebAPI
    Added getWeather() to fetch from Visual Crossing WebAPI. After the fetch
    is complete I call the sync function in the buttons event listener and use
    displayWeather() to append it to the output div. Currectly implemented only
    weatherObject.description

--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
 const btn = document.querySelector("button");
 const search = document.getElementById("cats");
+const weatherOutput = document.getElementById("output");
 
 async function getWeather(location) {
-  const response = await fetch(
-    "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/" +
-      encodeURIComponent(location) +
-      "?unitGroup=us&key=DM97HHAYYJMKM9SQ5X2U985K4&contentType=json",
-  );
+  try {
+    const response = await fetch(
+      "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/" +
+        encodeURIComponent(location) +
+        "?unitGroup=us&key=DM97HHAYYJMKM9SQ5X2U985K4&contentType=json",
+    );
 
-  const weatherData = await response.json();
-  console.log(weatherData);
+    const weatherData = await response.json();
+    const { currentConditions, description } = weatherData;
+    //remember to destruct curentConditions as well to get only what you want
+    return { currentConditions, description };
+    //console.log(weatherData.description);
+  } catch (error) {
+    //can use (error instancesof SyntaxError)
+    if (error.name === "SyntaxError") {
+      alert("Please enter a valid location");
+    } else alert(error);
+  }
+}
+function displayWeather(object) {
+  //console.log(object);
+  weatherOutput.textContent = object.description;
+  // /weatherOutput.appendChild(object);
 }
 
 btn.addEventListener("click", () => {
-  getWeather(search.value);
+  getWeather(search.value).then((response) => {
+    displayWeather(response);
+  });
+  //weatherOutput.appendChild(weatherInfo.description);
 });
