5b44d5d7ec3d349079674f20218f8e0d77fff4e2
whyitboi <20414206+whyitboi@users.noreply.github.com>
2026-09-18 14:28:47 +0300

    Add app.js as main app handler
    Implemented app.js to do all the event handling required by the app.
    index.js now just calls domLoad()  to load the elements and app() to
    handle the app state and events. Deleted remnants of todoList modules.

---
+++ src/app.js
@@ -0,0 +1,42 @@
+import { getWeather } from "./getWeather.js";
+import { processData } from "./processWeatherData.js";
+import { displayWeatherData } from "./displayWeatherData.js";
+
+function app() {
+  const form = document.querySelector("form");
+  const search = document.getElementById("searchBar");
+  const weatherOutput = document.getElementById("output");
+
+  const metric = document.getElementById("tempScale");
+  const metricDisplay = document.getElementById("switch");
+
+  metric.addEventListener("change", () => {
+    if (metric.value === "us") {
+      metric.value = "metric";
+    } else if (metric.value === "metric") {
+      metric.value = "us";
+    }
+
+    getWeather(search.value, metric.value).then((response) => {
+      if (response === undefined) return;
+      const processedData = processData(response);
+      displayWeatherData(processedData, metricDisplay, weatherOutput);
+    });
+  });
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    if (search.validity.valueMissing) {
+      search.setCustomValidity("Location cannot be empty");
+      search.reportValidity();
+      return;
+    } else search.setCustomValidity("");
+
+    getWeather(search.value, metric.value).then((response) => {
+      if (response === undefined) return;
+      const processedData = processData(response);
+      displayWeatherData(processedData, metricDisplay, weatherOutput);
+    });
+  });
+}
+export { app };
--- src/index.js
+++ src/index.js
@@ -1,44 +1,5 @@
 import { domLoad } from "./domLoad.js";
-import { getWeather } from "./getWeather.js";
-import { processData } from "./processWeatherData.js";
-import { displayWeatherData } from "./displayWeatherData.js";
+import { app } from "./app.js";
 
 domLoad();
-
-const form = document.querySelector("form");
-const search = document.getElementById("searchBar");
-const weatherOutput = document.getElementById("output");
-
-const metric = document.getElementById("tempScale");
-const metricDisplay = document.getElementById("switch");
-
-metric.addEventListener("change", () => {
-  if (metric.value === "us") {
-    metric.value = "metric";
-  } else if (metric.value === "metric") {
-    metric.value = "us";
-  }
-
-  getWeather(search.value, metric.value).then((response) => {
-    if (response === undefined) return;
-    const processedData = processData(response);
-    displayWeatherData(processedData, metricDisplay, weatherOutput);
-  });
-});
-
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
-  if (search.validity.valueMissing) {
-    search.setCustomValidity("Location cannot be empty");
-    search.reportValidity();
-    return;
-  } else search.setCustomValidity("");
-
-  getWeather(search.value, metric.value).then((response) => {
-    if (response === undefined) return;
-    const processedData = processData(response);
-    displayWeatherData(processedData, metricDisplay, weatherOutput);
-  });
-});
-
-export { metric };
+app();
